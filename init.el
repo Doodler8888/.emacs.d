e1814f62a4404a97425518e33a021eb92b797fa7
wurfkreuz <wurfkreuz@mail.ru>
2023-11-02 17:27:23 +0300

    n

--- init.el
+++ init.el
@@ -2,3 +2,18 @@
   (expand-file-name
     "config.org"
     user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((eval when
+	   (require 'rainbow-mode nil t)
+	   (rainbow-mode 1)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
