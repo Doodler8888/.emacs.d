84b5f9bf00c329544607f5585a7e7fb02b3bc36d
wurfkreuz <wurfkreuz@mail.ru>
2023-11-06 11:04:54 +0300

    n

--- tabsession.el
+++
@@ -1,8 +0,0 @@
-;; -*- mode: emacs-lisp; lexical-binding:t; coding: utf-8-emacs; -*-
-;; -------------------------------------------------------------------------
-;; Tabspaces Session File for Emacs
-;; -------------------------------------------------------------------------
-;; Created Fri Nov  3 15:42:34 2023
-
-;; Tabs and buffers:
-(setq tabspaces--session-list '((("/home/wurfkreuz/.emacs.d/config.org") . "config.org")))
