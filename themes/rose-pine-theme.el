5b7c789f4ea4b3be44ee31feddb1d06a342f2e43
wurfkreuz <wurfkreuz@mail.ru>
2024-04-10 00:49:42 +0300

    n

--- themes/rose-pine-theme.el
+++ themes/rose-pine-theme.el
@@ -132,7 +132,7 @@
 
    ;; More faces
    `(cursor ((,class (:foreground ,rose-pine-fg :background ,rose-pine-fg))))
-   `(show-paren-match ((t (:background ,rose-pine-subtext :foreground ,rose-pine-fg :weight bold))))  ; Customize matching bracket appearance
+   `(show-paren-match ((t (:background ,rose-pine-subtext :foreground ,rose-pine-fg :weight bold))))
    `(match ((,class (:background ,rose-pine-love :foreground ,rose-pine-bg))))
    `(minibuffer-prompt ((,class (:foreground ,rose-pine-iris))))
    `(trailing-whitespace ((,class (:background ,rose-pine-love))))
